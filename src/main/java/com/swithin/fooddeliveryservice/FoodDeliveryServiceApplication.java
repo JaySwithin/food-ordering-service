088df9b4bae2bcf927cb150198767bfb88ad3ad5
Swithin A. Okyere <jayswithin@gmail.com>
2023-06-21 21:32:51 +0000

    Initial commit - Set up application

---
+++ src/main/java/com/swithin/fooddeliveryservice/FoodDeliveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.swithin.fooddeliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDeliveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDeliveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swithin/fooddeliveryservice/FoodDeliveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swithin.fooddeliveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
